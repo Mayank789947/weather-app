c3e0086ab36e5c7d9ad97009f0d67560d523b1e6
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-02-10 19:24:47 +0530

    Added submit button functionality
    Added functionality on submit button to get location from user

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import { loadContent } from "./dom.js";
 import "./styles.css";
+import { createWeatherDataObj } from "./weatherData.js";
+
+const contentContainer = document.querySelector("#content");
+const input = document.querySelector("#location");
+const submitBtn = document.querySelector("#submit");
+
+
+submitBtn.addEventListener("click", async (e) => {
+
+    contentContainer.innerHTML = "";
+    const inputLocation = input.value.trim();
+
+    await loadContent(inputLocation);
+    input.value = "";
+})
