5027f14fc27e422a47a66d6bed3df8e27ab3893a
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-02-11 12:47:22 +0530

    Added loader while fetching data
    Added loader while fetching data from API call and hide it after getting data as well

--- src/index.js
+++ src/index.js
@@ -6,21 +6,34 @@ const contentContainer = document.querySelector("#content");
 const input = document.querySelector("#location");
 const submitBtn = document.querySelector("#submit");
 
+const loader = document.getElementById("loader");
+
+function showLoader() {
+  loader.classList.remove("hidden");
+}
+
+function hideLoader() {
+  loader.classList.add("hidden");
+}
+
 submitBtn.addEventListener("click", async (e) => {
 
-    contentContainer.innerHTML = "";
     const inputLocation = input.value.trim();
 
     if (!inputLocation) {
-        alert("Please enter correct location");
+        alert("Please Enter Correct Location");
         return
     }
 
     try {
+        showLoader();
+        contentContainer.innerHTML = "";
         await loadContent(inputLocation);
         input.value = "";
     } catch (error) {
         console.log(error);
+    } finally {
+        hideLoader();
     }
 
 })
